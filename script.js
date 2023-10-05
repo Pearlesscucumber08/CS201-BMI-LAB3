cfee6e982096826be3649c44916a842d4a8a630c
Ricardo Haduca <ibrigitteaeyahoo@gmail.com>
2023-10-05 13:32:59 +0800

    feat: added javascriptinto my html

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstName = " Brigitte Mae";
+let last_name = " Ibeng";
+let fullName = firstName + ' ' + last_name;
+console.log (fullName);
+console.log("BSCS-2A");
+console.log("CS201-BMI-LAB3");
+
+
+let password = prompt ("enter your password");
+let input =(password.length);
+if (input == 8){
+    console.log ("Welcome");
+}else if (input <= 8 && input === 1 ){
+    console.log ("Password is too short");
+}else if (input >= 8){
+    console.log("Password should be 8 characters");
+}else if (input == 0){
+    console.log("Please provide a password");
+}
